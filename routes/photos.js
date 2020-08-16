42b81aea5a84daf505e6787bf44569140dbff46a
Jay Baffoni <jay@baffoni5.com>
2020-08-16 11:15:06 -0400

    sign in routes

--- routes/photos.js
+++ routes/photos.js
@@ -1,38 +1,65 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var pool = require('./database')
+var pool = require("./database");
 
 //get image feed
-router.get('/', function(req, res, next) {
-    pool.query("SELECT * FROM photos ORDER BY timestamp desc", function(err, result){
-        if(err) throw err;
-        for(var i = 0; i < result.length; i++){
-            var photo = result[i];
-            console.log(photo.url);
-        }
-        res.send(result);
-    });
+router.get("/", function (req, res, next) {
+  pool.query("SELECT * FROM photos ORDER BY timestamp desc", function (
+    err,
+    result
+  ) {
+    if (err) throw err;
+    res.send(result);
+  });
 });
 
 //upload photo
-router.post('/', function(req, res, next) {
+router.post("/", function (req, res, next) {
+  // console.log(req.body);
 
-    // console.log(req.body);
+  var user_id = req.body.user_id;
+  var url = req.body.url;
+  var hidden = req.body.hidden;
+  var timestamp = new Date();
+  var caption = req.body.caption;
 
-    var user_id = req.body.user_id;
-    var url = req.body.url;
-    var hidden = req.body.hidden;  
-    var timestamp = new Date();
-    var caption = req.body.caption;  
+  pool.query(
+    "INSERT INTO photos (poster, url, timestamp, caption, hidden) VALUES (?, ?, ?, ?, ?)",
+    [user_id, url, timestamp, caption, hidden],
+    function (err, result) {
+      if (err) throw err;
+      for (var i = 0; i < result.length; i++) {
+        var photo = result[i];
+        console.log(photo.url);
+      }
+      res.send(result);
+    }
+  );
+});
 
-    pool.query("INSERT INTO photos (poster, url, timestamp, caption, hidden) VALUES (?, ?, ?, ?, ?)", [user_id, url, timestamp, caption, hidden], function(err, result){
-        if(err) throw err;
-        for(var i = 0; i < result.length; i++){
-            var photo = result[i];
-            console.log(photo.url);
-        }
-        res.send(result);
-    });
+router.post("/:photoid", function (req, res, next) {
+  // console.log(req.body);
+
+  var photoid = req.params.photoid;
+
+  // var user_id = req.body.user_id;
+  // var url = req.body.url;
+  // var hidden = req.body.hidden;
+  // var timestamp = new Date();
+  // var caption = req.body.caption;
+
+  pool.query(
+    "INSERT INTO photos (poster, url, timestamp, caption, hidden) VALUES (?, ?, ?, ?, ?)",
+    [user_id, url, timestamp, caption, hidden],
+    function (err, result) {
+      if (err) throw err;
+      for (var i = 0; i < result.length; i++) {
+        var photo = result[i];
+        console.log(photo.url);
+      }
+      res.send(result);
+    }
+  );
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,39 +1,81 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var pool = require("./database");
 
 //Sign In
-router.get('/', function(req, res, next) {
-  res.send('sign in');
+router.get("/", function (req, res, next) {
+  var email = req.body.email;
+  var input_password = req.body.password;
+
+  pool.query("SELECT * FROM users WHERE email=?", email, function (
+    err,
+    result
+  ) {
+    if (err) throw err;
+    if (result.length > 0) {
+      var db_password = result[0].password;
+      if (input_password === db_password) {
+        console.log("password ok");
+        res.send(result);
+      } else {
+        console.log("password wrong");
+        res.send('[{"error":"bad password"}]');
+      }
+    } else {
+      res.send('[{"error":"could not find user"}]');
+    }
+  });
 });
 
 //Register
-router.post('/', function(req, res, next) {
-  res.send('register');
+router.post("/", function (req, res, next) {
+  var email = req.body.email;
+  var password = req.body.password;
+  var displayName = req.body.displayName;
+  var showHidden = req.body.showHidden;
+
+  pool.query(
+    "INSERT INTO users (email, password, display_name, show_hidden) VALUES (?, ?, ?, ?)",
+    [email, password, displayName, showHidden],
+    function (err, result) {
+      if (err) res.send(err);
+      res.send(result);
+    }
+  );
 });
 
 //Change Password
-router.put('/password', function(req, res, next) {
-  res.send('change pw');
+router.put("/password", function (req, res, next) {
+  var user_id = req.body.user_id;
+  var password = req.body.password;
+  pool.query(
+    "UPDATE users SET password=? WHERE user_id=?",
+    [password, user_id],
+    function (err, result) {
+      if (err) res.send(err);
+      res.send(result);
+    }
+  );
 });
 
 //Change Email
-router.put('/email', function(req, res, next) {
-  res.send('change email');
+router.put("/email", function (req, res, next) {
+  res.send("change email");
 });
 
 //Change Name
-router.put('/name', function(req, res, next) {
-  res.send('change name');
+router.put("/name", function (req, res, next) {
+  res.send("change name");
 });
 
 //Toggle Hidden
-router.put('/hidden', function(req, res, next) {
-  res.send('toggle hidden content');
+router.put("/hidden", function (req, res, next) {
+  res.send("toggle hidden content");
 });
 
 //Get User Prefs
-router.get('/prefs', function(req, res, next) {
-  res.send('get user prefs');
+router.get("/prefs", function (req, res, next) {
+  res.send("get user prefs");
 });
 
 module.exports = router;
